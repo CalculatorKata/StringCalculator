ac9f8d9380701721952b0953afca83238e105dce
Wycliffe Mohapi <wmohapi@astutefse.com>
2019-01-04 10:53:54 +0200

    Step1 Commit

---
+++ stringcalculator/StringCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public int Add(string Input)
+        {
+            int result = 0;
+            return result;
+        }
+    }
+}
